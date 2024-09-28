6549700094631f2f38ba1c2ea5210284068b25a9
Ramin Karimpor <ramin.kp81@gmail.com>
2024-09-28 19:17:18 +0330

    caffeine project init

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="bg-red-500 text-zinc-800 text-center font-extrabold text-2xl">
+      Home
+    </div>
+  );
+}
+
+export default Home;
